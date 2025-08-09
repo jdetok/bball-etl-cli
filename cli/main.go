1083b4791bd70173a8bba9881c3094f44a48d6ef
jdetok <jdeko17@gmail.com>
2025-08-09 16:38:15 -0500

    project intent

--- cli/main.go
+++ cli/main.go
@@ -5,7 +5,23 @@ import (
 	"fmt"
 )
 
-// commment for testing
+/*
+PROJECT INTENT:
+  - create a command line interface for the bball-etl-go package to eliminate
+    the need for separate directories for the nightly/build runs of the etl
+*/
+
+/* TODO:
+- exit program if no arguments are passed (args slices == 1)
+- define a flag for mode:
+	- build: runs full etl all games 1970 through current
+	- daily: runs etl for games from previous day
+
+- LATER:
+	- define an additional flag for league
+	- eventually, define flags for different endpoint endpoints
+*/
+
 func main() {
 	// args := os.Args
 	var word string
